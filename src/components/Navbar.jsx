0cbd7ddb6ef0447a1459ab22681d649fbc6eea61
Daniel <dmunier27@gmail.com>
2024-01-02 02:13:47 -0300

    note: navbar to be fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 //import React from 'react'
 import "../styles/components/navbar.sass";
 import { Link, BrowserRouter } from "react-router-dom";
+import {GiHamburgerMenu} from "react-icons/gi"
 
 const Navbar = () => {
   return (
     <nav>
       <h2><a href="/">Daniel Munier</a></h2>
-
+    <div id="menu-button">
+    </div>
       <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#projects">Projetos</a></li>
