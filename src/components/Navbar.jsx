064c79844cd837e8f1b7c00ce72523d3c54d930d
Daniel <dmunier27@gmail.com>
2024-01-06 23:58:00 -0300

    new: responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,36 @@
 //import React from 'react'
 import "../styles/components/navbar.sass";
 import { Link, BrowserRouter } from "react-router-dom";
-import {GiHamburgerMenu} from "react-icons/gi"
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   return (
-    <nav>
-      <h2><a href="/">Daniel Munier</a></h2>
-    <div id="menu-button">
-    </div>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#projects">Projetos</a></li>
-        <li><a href="#contact">Contato</a></li>
-        <li><Link href="/">Projects</Link></li>
-      </ul>
-    </nav>
+    <header>
+     <div className="mobile">
+     <h2>
+        <a href="/">Daniel Munier</a>
+      </h2>
+      <div id="menu-button">
+        <GiHamburgerMenu/>
+      </div>
+     </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#projects">Projetos</a>
+          </li>
+          <li>
+            <a href="#contact">Contato</a>
+          </li>
+          <li>
+            <Link href="/">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
